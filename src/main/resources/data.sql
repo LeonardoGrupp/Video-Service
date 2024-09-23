13d1c2ddabaed324c7fd76a6cd264385d4744e58
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-09-23 21:26:19 +0200

    cleaned up data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,14 +10,6 @@ CREATE TABLE IF NOT EXISTS videos (
     )
 ;
 
--- CREATE TABLE IF NOT EXISTS video_genres (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     genre VARCHAR(255) NOT NULL,
---     total_likes INT DEFAULT 0,
---     total_plays INT DEFAULT 0
---     )
--- ;
-
 CREATE TABLE IF NOT EXISTS genres (
     id INT AUTO_INCREMENT PRIMARY KEY,
     genre VARCHAR(255) NOT NULL,
@@ -25,14 +17,6 @@ CREATE TABLE IF NOT EXISTS genres (
     total_plays INT DEFAULT 0
     )
 ;
-
--- CREATE TABLE IF NOT EXISTS videos_genres (
---     videos_id INT,
---     genre_id INT,
---     FOREIGN KEY(videos_id) REFERENCES videos(id),
---     FOREIGN KEY(genre_id) REFERENCES video_genres(id)
---     )
--- ;
 
 CREATE TABLE IF NOT EXISTS videos_genres (
     videos_id INT,
@@ -59,13 +43,6 @@ CREATE TABLE IF NOT EXISTS videos_albums (
 ;
 
 -- INSERT Genres
--- INSERT INTO video_genres (id, genre, total_likes, total_plays)
--- VALUES
---     (1, 'Rock', 0, 0),
---     (2, 'Heavy Metal', 0, 0)
--- ;
-
--- INSERT Genres
 INSERT INTO genres (id, genre, total_likes, total_plays)
 VALUES
     (19, 'Rock', 0, 0),
@@ -74,49 +51,19 @@ VALUES
 
 INSERT INTO videos (id, type, title, url, release_date, play_counter, likes, dis_likes)
 VALUES
-    -- MUSIC
-    (1, 'video', 'The Wicker Man', 'url39', '2002', 0, 0, 0),
-    (2, 'video','Ghost Of the Navigator', 'url40', '2002', 0, 0, 0),
-    (3, 'video', 'Brave New World', 'url41', '2002', 0, 0, 0),
-    (4, 'video', 'Wrathchild', 'url42', '2002', 0, 0, 0),
-    (5, 'video', '2 Minutes To Midnight', 'url43', '2002', 0, 0, 0),
-    (6, 'video', 'Blood Brothers', 'url44', '2002', 0, 0, 0),
-    (7, 'video', 'Sign Of the Cross', 'url45', '2002', 0, 0, 0),
-    (8, 'video', 'The Mercenary', 'url46', '2002', 0, 0, 0),
-    (9, 'video', 'The Trooper', 'url47', '2002', 0, 0, 0),
-    (10, 'video', 'Dream Of Mirrors', 'url48', '2002', 0, 0, 0),
-    (11, 'video', 'The Clansman', 'url49', '2002', 0, 0, 0),
-    (12, 'video', 'The Evil That Men Do', 'url50', '2002', 0, 0, 0),
-    (13, 'video', 'Fear Of the Dark', 'url51', '2002', 0, 0, 0),
-    (14, 'video', 'Iron Maiden', 'url52', '2002', 0, 0, 0),
-    (15, 'video', 'The Number Of the Beast', 'url53', '2002', 0, 0, 0),
-    (16, 'video', 'Hallowed Be Thy Name', 'url54', '2002', 0, 0, 0),
-    (17, 'video', 'Sanctuary', 'url55', '2002', 0, 0, 0),
-    (18, 'video', 'Run To the Hills', 'url56', '2002', 0, 0, 0),
+    (1, 'video', 'The Wicker Man', 'url39', '2002', 0, 0, 0), (2, 'video','Ghost Of the Navigator', 'url40', '2002', 0, 0, 0), (3, 'video', 'Brave New World', 'url41', '2002', 0, 0, 0), (4, 'video', 'Wrathchild', 'url42', '2002', 0, 0, 0), (5, 'video', '2 Minutes To Midnight', 'url43', '2002', 0, 0, 0), (6, 'video', 'Blood Brothers', 'url44', '2002', 0, 0, 0), (7, 'video', 'Sign Of the Cross', 'url45', '2002', 0, 0, 0), (8, 'video', 'The Mercenary', 'url46', '2002', 0, 0, 0), (9, 'video', 'The Trooper', 'url47', '2002', 0, 0, 0), (10, 'video', 'Dream Of Mirrors', 'url48', '2002', 0, 0, 0), (11, 'video', 'The Clansman', 'url49', '2002', 0, 0, 0), (12, 'video', 'The Evil That Men Do', 'url50', '2002', 0, 0, 0), (13, 'video', 'Fear Of the Dark', 'url51', '2002', 0, 0, 0), (14, 'video', 'Iron Maiden', 'url52', '2002', 0, 0, 0), (15, 'video', 'The Number Of the Beast', 'url53', '2002', 0, 0, 0), (16, 'video', 'Hallowed Be Thy Name', 'url54', '2002', 0, 0, 0), (17, 'video', 'Sanctuary', 'url55', '2002', 0, 0, 0), (18, 'video', 'Run To the Hills', 'url56', '2002', 0, 0, 0),
 
-    (19, 'video', 'Once Upon A Time in the West', 'url57', '1984', 0, 0, 0),
-    (20, 'video', 'Romeo And Juliet', 'url58', '1984', 0, 0, 0),
-    (21, 'video', 'Expresso Love', 'url59', '1984', 0, 0, 0),
-    (22, 'video', 'Private Investigations', 'url60', '1984', 0, 0, 0),
-    (23, 'video', 'Sultans Of Swing', 'url61', '1984', 0, 0, 0),
-    (24, 'video', 'Tunnel Of Love', 'url62', '1984', 0, 0, 0),
-    (25, 'video', 'Telegraph Road', 'url63', '1984', 0, 0, 0),
-    (26, 'video', 'Solid Rock', 'url64', '1984', 0, 0, 0),
-    (27, 'video', 'Going Home - Theme From ''Local Hero''', 'url65', '1984', 0, 0, 0);
+    (19, 'video', 'Once Upon A Time in the West', 'url57', '1984', 0, 0, 0), (20, 'video', 'Romeo And Juliet', 'url58', '1984', 0, 0, 0), (21, 'video', 'Expresso Love', 'url59', '1984', 0, 0, 0), (22, 'video', 'Private Investigations', 'url60', '1984', 0, 0, 0), (23, 'video', 'Sultans Of Swing', 'url61', '1984', 0, 0, 0), (24, 'video', 'Tunnel Of Love', 'url62', '1984', 0, 0, 0), (25, 'video', 'Telegraph Road', 'url63', '1984', 0, 0, 0), (26, 'video', 'Solid Rock', 'url64', '1984', 0, 0, 0), (27, 'video', 'Going Home - Theme From ''Local Hero''', 'url65', '1984', 0, 0, 0);
 ;
 
 INSERT INTO videos_genres (videos_id, genre_id)
 VALUES
     -- VIDEOS
     -- Iron Maiden to Metal
-    (1, 20), (2, 20), (3, 20), (4, 20), (5, 20),
-    (6, 20), (7, 20), (8, 20), (9, 20), (10, 20),
-    (11, 20), (12, 20), (13, 20), (14, 20), (15, 20),
-    (16, 20), (17, 20), (18, 20),
+    (1, 20), (2, 20), (3, 20), (4, 20), (5, 20), (6, 20), (7, 20), (8, 20), (9, 20), (10, 20), (11, 20), (12, 20), (13, 20), (14, 20), (15, 20), (16, 20), (17, 20), (18, 20),
 
     --Dire Straits
-    (19, 19), (20, 19), (21, 19), (22, 19), (23, 19),
-    (24, 19), (25, 19), (26, 19), (27, 19)
+    (19, 19), (20, 19), (21, 19), (22, 19), (23, 19), (24, 19), (25, 19), (26, 19), (27, 19)
 ;
 
 -- INSERT Artists
@@ -124,39 +71,10 @@ INSERT INTO videos_artists (videos_id, artist_id)
 VALUES
     -- VIDEOS
     --Rock in Rio
-    (1, 9), -- Iron Maiden
-    (2, 9), -- Iron Maiden
-    (3, 9), -- Iron Maiden
-    (4, 9), -- Iron Maiden
-    (5, 9), -- Iron Maiden
-    (6, 9), -- Iron Maiden
-    (7, 9), -- Iron Maiden
-    (8, 9), -- Iron Maiden
-    (9, 9), -- Iron Maiden
-    (10, 9), -- Iron Maiden
-    (11, 9), -- Iron Maiden
-    (12, 9), -- Iron Maiden
-    (13, 9), -- Iron Maiden
-    (14, 9), -- Iron Maiden
-    (15, 9), -- Iron Maiden
-    (16, 9), -- Iron Maiden
-    (17, 9), -- Iron Maiden
-    (18, 9), -- Iron Maiden
-    -- Features
-    --(1, 2, 'Lisa Miskovsky'),
+    (1, 9), (2, 9), (3, 9), (4, 9), (5, 9), (6, 9), (7, 9), (8, 9), (9, 9), (10, 9), (11, 9), (12, 9), (13, 9), (14, 9), (15, 9), (16, 9), (17, 9), (18, 9), -- Iron Maiden
 
     --Alchemy
-    (19, 10), -- Dire Straits
-    (20, 10), -- Dire Straits
-    (21, 10), -- Dire Straits
-    (22, 10), -- Dire Straits
-    (23, 10), -- Dire Straits
-    (24, 10), -- Dire Straits
-    (25, 10), -- Dire Straits
-    (26, 10), -- Dire Straits
-    (27, 10) -- Dire Straits
-    -- Features
-    --(19, 5, 'Jeezy'),
+    (19, 10), (20, 10), (21, 10), (22, 10), (23, 10), (24, 10), (25, 10), (26, 10), (27, 10) -- Dire Straits
 ;
 
 -- INSERT ALbums
@@ -164,33 +82,8 @@ INSERT INTO videos_albums (videos_id, album_id)
 VALUES
     -- VIDEO
     -- Iron Maiden
-    (1, 4), -- Rock in Rio
-    (2, 4), -- Rock in Rio
-    (3, 4), -- Rock in Rio
-    (4, 4), -- Rock in Rio
-    (5, 4), -- Rock in Rio
-    (6, 4), -- Rock in Rio
-    (7, 4), -- Rock in Rio
-    (8, 4), -- Rock in Rio
-    (9, 4), -- Rock in Rio
-    (10, 4), -- Rock in Rio
-    (11, 4), -- Rock in Rio
-    (12, 4), -- Rock in Rio
-    (13, 4), -- Rock in Rio
-    (14, 4), -- Rock in Rio
-    (15, 4), -- Rock in Rio
-    (16, 4), -- Rock in Rio
-    (17, 4), -- Rock in Rio
-    (18, 4), -- Rock in Rio
+    (1, 4), (2, 4), (3, 4), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (9, 4), (10, 4), (11, 4), (12, 4), (13, 4), (14, 4), (15, 4), (16, 4), (17, 4), (18, 4), -- Rock in Rio
 
     -- Dire Straits
-    (19, 5), -- Alchemy
-    (20, 5), -- Alchemy
-    (21, 5), -- Alchemy
-    (22, 5), -- Alchemy
-    (23, 5), -- Alchemy
-    (24, 5), -- Alchemy
-    (25, 5), -- Alchemy
-    (26, 5), -- Alchemy
-    (27, 5) -- Alchemy
+    (19, 5), (20, 5), (21, 5), (22, 5), (23, 5), (24, 5), (25, 5), (26, 5), (27, 5) -- Alchemy
 ;
